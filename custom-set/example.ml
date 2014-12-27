f8023366d8679d921c57b7bc322c9b4c69efb6d5
Peter Minten <peter@pminten.nl>
2014-12-27 13:48:11 +0100

    Add implementation of custom-set exercise

---
+++ custom-set/example.ml
@@ -0,0 +1,83 @@
+module type ELEMENT = sig
+    type t
+    val compare : t -> t -> int
+    val equal : t -> t -> bool
+    val to_string : t -> string
+end
+
+module Make(El: ELEMENT) = struct
+    (* Not terribly efficient, but good enough for this example.
+     *
+     * The list is kept sorted at all times. *)
+    type t = El.t list
+    
+    type el = El.t
+    
+    let compare a b =
+        let rec comp = function
+            | [], [] -> 0
+            | (_::_), [] -> -1
+            | [], (_::_) -> 1
+            | (h1::t1), (h2::t2) ->
+                match compare h1 h2 with
+                | 0 -> comp (t1, t2)
+                | v -> v
+        in comp (a, b)
+
+    let equal a b = compare a b == 0
+    
+    let to_string l = 
+        let rec print_els = function
+            | (h1::h2::t) -> El.to_string h1 ^ " " ^ print_els (h2::t)
+            | (h1::t) -> El.to_string h1 ^ print_els t
+            | [] -> "" in
+        "{" ^ print_els l ^ "}"
+
+    let empty = []
+    
+    let of_list l = List.sort_uniq El.compare l
+    
+    let to_list l = l 
+
+    let add l x =
+        let rec go acc = function
+            | h :: t ->
+                let r = El.compare x h in
+                if r = 0 then l (* element already in the set *)
+                else if r < 0 then List.rev_append acc (x :: h :: t)
+                else go (h::acc) t
+            | [] -> List.rev (x::acc)
+        in go [] l
+
+    let remove l x =
+        let rec go acc = function
+            | h :: t ->
+                let r = El.compare x h in
+                if r = 0 then List.rev_append acc t
+                else if r < 0 then l
+                else go (h::acc) t
+            | [] -> l 
+        in go [] l
+
+    type status = [
+        | `OnlyA
+        | `OnlyB
+        | `Both
+    ]
+
+    let diff_filter (f : status -> bool) l1 l2 =
+        let rec go acc = function
+            | [], [] -> List.rev acc
+            | (h1::t1), [] -> go (if f `OnlyA then h1::acc else acc) (t1, [])
+            | [], (h2::t2) -> go (if f `OnlyB then h2::acc else acc) ([], t2)
+            | (h1::t1), (h2::t2) ->
+                let r = El.compare h1 h2 in
+                if r == 0 then go (if f `Both then h1::acc else acc) (t1, t2)
+                else if r < 0 then go (if f `OnlyA then h1::acc else acc) (t1, h2::t2)
+                else go (if f `OnlyB then h2::acc else acc) (h1::t1, t2)
+        in go [] (l1, l2)
+
+    let difference = diff_filter (function `OnlyA -> true | _ -> false)
+    let intersect = diff_filter (function `Both -> true | _ -> false)
+    let union = diff_filter (fun _ -> true)
+end
---
+++ custom-set/test.ml
@@ -0,0 +1,80 @@
+open OUnit2
+
+module type EXPECTED = sig
+    type t
+    val empty : t
+    val equal : t -> t -> bool
+    val compare : t -> t -> int
+    val to_string : t -> string
+    val of_list : int list -> t
+    val to_list : t -> int list
+    val add : t -> int -> t
+    val remove : t -> int -> t
+    val difference : t -> t -> t
+    val intersect : t -> t -> t
+    val union : t -> t -> t
+end
+
+module CSet : EXPECTED = Custom_set.Make(struct
+    type t = int
+    let compare a b = compare (a mod 10) (b mod 10)
+    let equal a b = compare a b == 0 
+    let to_string v = string_of_int v
+end)
+
+let rec int_list_eq l1 l2 = match (l1, l2) with
+    | (h1::t1), (h2::t2) -> h1 == h2 && int_list_eq t1 t2
+    | [], [] -> true
+    | _, _ -> false
+
+let int_list_printer l = 
+    let rec print_els = function
+        | (h1::h2::t) -> string_of_int h1 ^ " " ^ print_els (h2::t)
+        | (h1::t) -> string_of_int h1 ^ print_els t
+        | [] -> "" in
+    "[" ^ print_els l ^ "]"
+    
+let ae_set exp got _test_ctxt = assert_equal ~cmp:CSet.equal ~printer:CSet.to_string exp got
+let ae_list exp got _test_ctxt = assert_equal ~cmp:int_list_eq ~printer:int_list_printer exp got
+let ae_repr exp set _test_ctxt = assert_equal ~cmp:(=) ~printer:(fun x -> x) exp (CSet.to_string set)
+
+let tests = [
+    "empty">::
+        ae_repr "{}" (CSet.empty);
+    "empty = empty">::
+        ae_set (CSet.empty) (CSet.empty);
+    "of_list - no duplicates">::
+        ae_repr "{1 2 3}" (CSet.of_list [2;3;1]);
+    "of_list - duplicates">::
+        ae_repr "{1 2 3}" (CSet.of_list [2;3;2;1;1]);
+    "to_list">::
+        ae_list [-2; 1; 3] (CSet.to_list (CSet.of_list [3;1;-2]));
+    "add non-duplicate">::
+        ae_repr "{1 3 4}" (CSet.add (CSet.of_list [1;4]) 3);
+    "add duplicate">::
+        ae_repr "{1 3 4}" (CSet.add (CSet.of_list [1;4;3]) 3);
+    "add onto empty set">::
+        ae_repr "{3}" (CSet.add CSet.empty 3);
+    "remove found">::
+        ae_repr "{1 4}" (CSet.remove (CSet.of_list [1;3;4]) 3);
+    "remove not found">::
+        ae_repr "{1 4}" (CSet.remove (CSet.of_list [1;4]) 3);
+    "remove from empty set">::
+        ae_repr "{}" (CSet.remove CSet.empty 3);
+    "custom compare">::
+        ae_list [1; 2; 3]
+          (List.map (fun x -> x mod 10) (CSet.to_list
+             (CSet.remove (CSet.add (CSet.of_list [103; 11; 22; 404]) 53) 14)));
+    "difference">::
+        ae_repr "{1 3 5}"
+          (CSet.difference (CSet.of_list [1; 2; 3; 4; 5]) (CSet.of_list [2; 4]));
+    "intersect">::
+        ae_repr "{1 3}"
+          (CSet.intersect (CSet.of_list [1; 2; 3]) (CSet.of_list [3; 5; 1]));
+    "union">::
+        ae_repr "{1 2 3 5}"
+          (CSet.union (CSet.of_list [1; 2; 3]) (CSet.of_list [3; 5; 1]));
+    ]
+
+let () =
+  run_test_tt_main ("custom_set tests" >::: tests)
