99bea64033136d3345846a012b78404d9b5a4a8e
Aaron L. Zeng <a2z@mit.edu>
2016-06-29 23:30:01 -0400

    make hamming fail for unequal length strands

--- exercises/hamming/hamming.mli
+++ exercises/hamming/hamming.mli
@@ -3,4 +3,4 @@
 type nucleotide = A | C | G | T
 
 (** Compute the hammning distance between the two lists. *)
-val hamming_distance : nucleotide list -> nucleotide list -> int
+val hamming_distance : nucleotide list -> nucleotide list -> int option
