4a2a1318614e8475a9443fb1601311949a321378
Vasile Rotaru <vrotaru.md@gmail.com>
2016-09-10 22:44:28 +0300

    Roman Numeral exercise for OCaml track

---
+++ exercises/roman-numerals/example.ml
@@ -0,0 +1,23 @@
+
+let to_roman n = 
+  assert (n <= 3000);
+
+  let build ones halves tens = function
+    | 0 -> ""
+    | 1 -> ones
+    | 2 -> ones ^ ones
+    | 3 -> ones ^ ones ^ ones
+    | 4 -> ones ^ halves
+    | 5 -> halves
+    | 6 -> halves ^ ones
+    | 7 -> halves ^ ones ^ ones
+    | 8 -> halves ^ ones ^ ones ^ ones
+    | 9 -> ones ^ tens
+    | _ -> assert false
+  in
+  let thousands n = build "M" "" "" (n / 1000 mod 10) in
+  let hundreds n  = build "C" "D" "M" (n / 100 mod 10) in
+  let tens n      = build "X" "L" "C" (n / 10 mod 10) in
+  let ones n      = build "I" "V" "X" (n mod 10) in
+
+  thousands n ^ hundreds n ^ tens n ^ ones n
---
+++ exercises/roman-numerals/roman_numerals.mli
@@ -0,0 +1,4 @@
+(* Roman numerals exercise *)
+
+(** Convert an integer value to its Roman Numeral representation up to 3000 inclusive *)
+val to_roman : int -> string
