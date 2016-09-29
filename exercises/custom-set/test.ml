7efe08ffd542c58c1d4add669210b5729f752f8b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-09-29 21:28:13 +0200

    Update tests to reflect all common tests

--- exercises/custom-set/test.ml
+++ exercises/custom-set/test.ml
@@ -6,6 +6,8 @@ module type EXPECTED = sig
   val equal : t -> t -> bool
   val is_empty : t -> bool
   val is_member : t -> int -> bool
+  val is_subset : t -> t -> bool
+  val is_disjoint: t -> t -> bool
   val to_string : t -> string
   val of_list : int list -> t
   val to_list : t -> int list
@@ -43,89 +45,80 @@ let assert_false exp _text_ctxt = assert_equal exp false
 let assert_not_equal exp got _test_ctxt = assert_equal ~cmp:(fun x y -> (CSet.equal x y) |> not) exp got
 
 let tests = [
-  "empty">::
-    ae_repr "{}" (CSet.empty);
-  "is_empty true on the empty set">::
-    assert_true (CSet.is_empty CSet.empty);
-  "is_empty false on a non-empty set">::
-    assert_false (CSet.is_empty (CSet.of_list [1]));
-  "is_empty true on a set created from an empty list">::
+  "sets with no elements are empty">::
     assert_true (CSet.is_empty (CSet.of_list []));
-  "empty = empty">::
-    ae_set (CSet.empty) (CSet.empty);
-  "empty != non-empty">::
-    assert_not_equal CSet.empty (CSet.of_list [2]);
-  "non-empty != empty">::
-    assert_not_equal (CSet.of_list [2]) CSet.empty;
-  "equal - different non-empty sets">::
-    assert_not_equal (CSet.of_list [2]) (CSet.of_list [3]);
-  "equal - equal non-empty sets">::
-    ae_set (CSet.of_list [2]) (CSet.of_list [2]);
-  "equal - equal sets using the provided equal method">::
-    ae_set (CSet.of_list [2]) (CSet.of_list [12]);
-  "is_member false on the empty set">::
-    assert_false (CSet.is_member CSet.empty 3);
-  "is_member false if not a member">::
-    assert_false (CSet.is_member (CSet.of_list [1;2;5]) 3);
-  "is_member true if a member">::
-    assert_true (CSet.is_member (CSet.of_list [1;3;5]) 3);
-  "is_member true if a member using the provided equal method">::
-    assert_true (CSet.is_member (CSet.of_list [1;3;5]) 13);
-  "of_list - no duplicates">::
-    ae_repr "{1 2 3}" (CSet.of_list [2;3;1]);
-  "of_list - duplicates">::
-    ae_repr "{1 2 3}" (CSet.of_list [2;3;2;1;1]);
-  "of_list - no duplicates using the provided equal method">::(fun _test_context ->
-    let s = (CSet.of_list [2;3;1;11;13;12]) in
-    assert_bool "1 (= 11 mod 10) in the set" (CSet.is_member s 1);
-    assert_bool "2 (= 12 mod 10) in the intersection" (CSet.is_member s 2);
-    assert_bool "3 (= 13 mod 10) in the intersection" (CSet.is_member s 3);
-    assert_equal 3 (CSet.to_list s |> List.length);
-    );
-  "to_list">::
-    ae_list [-2; 1; 3] (CSet.to_list (CSet.of_list [3;1;-2]));
-  "add non-duplicate">::
-    ae_repr "{1 3 4}" (CSet.add (CSet.of_list [1;4]) 3);
-  "add duplicate">::
-    ae_repr "{1 3 4}" (CSet.add (CSet.of_list [1;4;3]) 3);
-  "add duplicate with respect to the provided equal method">::
-    ae_repr "{1 3 4}" (CSet.add (CSet.of_list [1;4;3]) 14);
-  "add onto empty set">::
-    ae_repr "{3}" (CSet.add CSet.empty 3);
-  "remove found">::
-    ae_repr "{1 4}" (CSet.remove (CSet.of_list [1;3;4]) 3);
-  "remove found with respect to the provided equal method">::
-    ae_repr "{1 4}" (CSet.remove (CSet.of_list [1;3;4]) 13);
-  "remove not found">::
-    ae_repr "{1 4}" (CSet.remove (CSet.of_list [1;4]) 3);
-  "remove from empty set">::
-    ae_repr "{}" (CSet.remove CSet.empty 3);
-  "difference">::
-    ae_repr "{1 3 5}"
-      (CSet.difference (CSet.of_list [1; 2; 3; 4; 5]) (CSet.of_list [2; 4]));
-  "difference using the provided equal method">::
-    ae_repr "{1 3 5}"
-      (CSet.difference (CSet.of_list [1; 2; 3; 4; 5]) (CSet.of_list [12; 14]));
-  "intersect">::
-    ae_repr "{1 3}"
-      (CSet.intersect (CSet.of_list [1; 2; 3]) (CSet.of_list [3; 5; 1]));
-  "intersect using the provided equal method">::(fun _test_context ->
-    let intersection = CSet.intersect (CSet.of_list [1; 2; 3]) (CSet.of_list [23; 15; 11]) in
-    assert_bool "1 (= 11 mod 10) in the intersection" (CSet.is_member intersection 1);
-    assert_bool "3 (= 23 mod 10) in the intersection" (CSet.is_member intersection 3);
-    assert_equal 2 (CSet.to_list intersection |> List.length);
-    );
-  "union">::
-    ae_repr "{1 2 3 5}"
-      (CSet.union (CSet.of_list [1; 2; 3]) (CSet.of_list [3; 5; 1]));
-  "union using the provided equal method">::(fun _test_context ->
-    let union = CSet.union (CSet.of_list [1; 2; 3]) (CSet.of_list [3; 5; 21]) in
-    assert_bool "1 (= 11 mod 10) in the union" (CSet.is_member union 1);
-    assert_bool "2 in the union" (CSet.is_member union 2);
-    assert_bool "3 (= 23 mod 10) in the union" (CSet.is_member union 3);
-    assert_bool "5 in the union" (CSet.is_member union 5);
-    assert_equal 4 (CSet.to_list union |> List.length);
-    );
+  "sets with elements are not empty">::
+    assert_false (CSet.is_empty (CSet.of_list [1]));
+  "nothing is contained in the empty set">::
+    assert_false (CSet.is_member (CSet.of_list []) 1);
+  "when the element is in the set">::
+    assert_true (CSet.is_member (CSet.of_list [1;2;3]) 1);
+  "when the element is not in the set">::
+    assert_false (CSet.is_member (CSet.of_list [1;3;3]) 4);
+  "empty set is a subset of an other empty set">::
+    assert_true (CSet.is_subset (CSet.of_list []) (CSet.of_list []));
+  "empty set is a subset of a non empty set">::
+    assert_true (CSet.is_subset (CSet.of_list []) (CSet.of_list [1]));
+  "non-empty set is a not subset of an empty set">::
+    assert_false (CSet.is_subset (CSet.of_list [1]) (CSet.of_list []));
+  "set is a subset of set with exact same elements">::
+    assert_true (CSet.is_subset (CSet.of_list [1;2;3]) (CSet.of_list [1;2;3]));
+  "set is a subset of larger set with exact same elements">::
+    assert_true (CSet.is_subset (CSet.of_list [1;2;3]) (CSet.of_list [4;1;2;3]));
+  "set is not a subset of set that does not contain its elements">::
+    assert_false (CSet.is_subset (CSet.of_list [1;2;3]) (CSet.of_list [4;1;3]));
+  "the empty set is disjoint with itself">::
+    assert_true (CSet.is_disjoint (CSet.of_list []) (CSet.of_list []));
+  "the empty set is disjoint with non-empty set">::
+    assert_true (CSet.is_disjoint (CSet.of_list []) (CSet.of_list [1]));
+  "non-empty set is disjoint with empty set">::
+    assert_true (CSet.is_disjoint (CSet.of_list [1]) (CSet.of_list []));
+  "sets are not disjoint if they share an element">::
+    assert_false (CSet.is_disjoint (CSet.of_list [1;2]) (CSet.of_list [2;3]));
+  "sets are disjoint if they do not share an element">::
+    assert_true (CSet.is_disjoint (CSet.of_list [1;2]) (CSet.of_list [3;4]));
+  "empty sets are equal">::
+    assert_true (CSet.equal (CSet.of_list []) (CSet.of_list []));
+  "empty set is not equal to non-empty set">::
+    assert_false (CSet.equal (CSet.of_list []) (CSet.of_list [1;2;3]));
+  "non-empty set is not equal to empty set">::
+    assert_false (CSet.equal (CSet.of_list [1;2;3]) (CSet.of_list []));
+  "sets with the same elements are equal">::
+    assert_true (CSet.equal (CSet.of_list [1;2]) (CSet.of_list [2;1]));
+  "sets with different elements are not equal">::
+    assert_false (CSet.equal (CSet.of_list [1;2;3]) (CSet.of_list [1;2;4]));
+  "add to empty set">::
+    assert_true (CSet.equal (CSet.of_list [3]) (CSet.add (CSet.of_list []) 3));
+  "add to non-empty set">::
+    assert_true (CSet.equal (CSet.of_list [1;2;3;4]) (CSet.add (CSet.of_list [1;2;4]) 3));
+  "adding existing element does not change set">::
+    assert_true (CSet.equal (CSet.of_list [1;2;3]) (CSet.add (CSet.of_list [1;2;3]) 3));
+  "intersection of two empty sets is empty set">::
+    assert_true (CSet.equal (CSet.of_list []) (CSet.intersect (CSet.of_list []) (CSet.of_list [])));
+  "intersection of empty set with non-empty set is an empty set">::
+    assert_true (CSet.equal (CSet.of_list []) (CSet.intersect (CSet.of_list []) (CSet.of_list [3;2;5])));
+  "intersection of non-empty set with empty set is an empty set">::
+    assert_true (CSet.equal (CSet.of_list []) (CSet.intersect (CSet.of_list [1;2;3;4]) (CSet.of_list [])));
+  "intersection of sets with no shared elements is empty set">::
+    assert_true (CSet.equal (CSet.of_list []) (CSet.intersect (CSet.of_list [1;2;3]) (CSet.of_list [4;5;6])));
+  "intersection of set with shared elements is set of shared elements">::
+    assert_true (CSet.equal (CSet.of_list [2;3]) (CSet.intersect (CSet.of_list [1;2;3;4]) (CSet.of_list [3;2;5])));
+  "difference of two empty sets is an empty set">::
+    assert_true (CSet.equal (CSet.of_list []) (CSet.difference (CSet.of_list []) (CSet.of_list [])));
+  "difference of empty set and non-empty set is empty set">::
+    assert_true (CSet.equal (CSet.of_list []) (CSet.difference (CSet.of_list []) (CSet.of_list [3;2;5])));
+  "difference of non-empty set and empty set is the non-empty set">::
+    assert_true (CSet.equal (CSet.of_list [1;2;3;4]) (CSet.difference (CSet.of_list [1;2;3;4]) (CSet.of_list [])));
+  "difference of two non-empty sets is the sets of elements only in the first set">::
+    assert_true (CSet.equal (CSet.of_list [1;3]) (CSet.difference (CSet.of_list [3;2;1]) (CSet.of_list [2;4])));
+  "union of two empty sets is an empty set">::
+    assert_true (CSet.equal (CSet.of_list []) (CSet.union (CSet.of_list []) (CSet.of_list [])));
+  "union of empty set and non-empty set is non-empty set">::
+    assert_true (CSet.equal (CSet.of_list [2]) (CSet.union (CSet.of_list []) (CSet.of_list [2])));
+  "union of non-empty set and empty set is the non-empty set">::
+    assert_true (CSet.equal (CSet.of_list [1;3]) (CSet.union (CSet.of_list [1;3]) (CSet.of_list [])));
+  "union of two non-empty sets contains all unique elements">::
+    assert_true (CSet.equal (CSet.of_list [1;2;3]) (CSet.union (CSet.of_list [1;3]) (CSet.of_list [2;3])));
   ]
 
 let () =
