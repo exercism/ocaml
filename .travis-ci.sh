59d7d1298bb009a84a0b97669a91a459b57dfe16
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-10-03 15:47:30 +0200

    Install core
    This enables corebuild

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind ounit"
+OPAM_DEPENDS="ocamlfind core ounit"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
     3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
