0e51921b20b62258584dbb5663516826d8f06afa
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-10-05 13:56:06 +0200

    Create difference-of-squares exercise

---
+++ exercises/difference-of-squares/difference_of_squares.mli
@@ -0,0 +1,8 @@
+(* Square the sum of the numbers up to the given number *)
+val square_of_sum : int -> int
+
+(* Sum the squares of the numbers up to the given number *)
+val sum_of_squares : int -> int
+
+(* Subtract sum of squares from square of sums *)
+val difference_of_squares : int -> int
