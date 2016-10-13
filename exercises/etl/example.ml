671997ab3f88797b0b6b324acb6b7b21ca8bbeba
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-10-13 15:33:46 +0200

    Lowercase the character in the output

--- exercises/etl/example.ml
+++ exercises/etl/example.ml
@@ -1,7 +1,7 @@
 open Core.Std
 
 let transform data =
-  let assign_value value character = (character, value) in
+  let assign_value value character = ((Char.lowercase character), value) in
   let assign (value, characters) = List.map ~f:(assign_value value) characters in
   let compare (a, _) (b, _) = Char.compare a b in
   List.sort compare (List.concat (List.map ~f:assign data))
