6e145dde7dd264211c5ba4d625056b576d22aaeb
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-10-13 13:00:20 +0200

    Create a printer

--- exercises/etl/test.ml
+++ exercises/etl/test.ml
@@ -2,7 +2,11 @@ open Core.Std
 open OUnit2
 open Etl
 
-let ae exp got _test_ctxt = assert_equal exp got
+
+let ae exp got _test_ctxt =
+  let sexpr_of l = List.sexp_of_t (Tuple2.sexp_of_t Char.sexp_of_t Int.sexp_of_t) l in
+  let printer l = Sexp.to_string (sexpr_of l) in
+  assert_equal exp got ~printer
 
 let tests =
   ["transform one character">::
