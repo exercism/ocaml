9c081791e3337e6b8e21c5b9b7f6b0edcbf8d5e9
Steve <stevejb71@gmail.com>
2016-11-21 21:08:44 +0800

    Sync anagram test with canonical data.

--- exercises/anagram/test.ml
+++ exercises/anagram/test.ml
@@ -6,29 +6,40 @@ let ae exp got _test_ctxt =
   let printer = List.to_string ~f:String.to_string in
   assert_equal exp got ~cmp:(List.equal ~equal:String.equal) ~printer
 
-let tests =
-  ["no matches">::
-     ae [] (anagrams "diaper" ["hello"; "world"; "zombies"; "pants"]);
-   "detect simple anagrams">::
-     ae ["tan"] (anagrams "ant" ["tan"; "stand"; "at"]);
-   "detect multiple anagrams">::
-     ae ["stream"; "maters"] (anagrams "master" ["stream"; "pigeon"; "maters"]);
-   "do not detect anagram subsets">::
-     ae [] (anagrams "good" ["dog"; "goody"]);
-   "multiple anagrams">::
-     ae ["gallery"; "regally"; "largely"]
-       (anagrams "allergy" ["gallery"; "ballerina"; "regally"; "clergy";
-                            "largely"; "leading"]);
-   "detect anagrams case-insensitively">::
-     ae ["Carthorse"]
-       (anagrams "Orchestra" ["cashregister"; "Carthorse"; "radishes"]);
-   "anagrams must not be the source word">::
-     ae [] (anagrams "banana" ["banana"]);
-   "anagrams must not be the source word case-insensitively">::
-     ae [] (anagrams "banana" ["Banana"]);
-   "anagrams must use all letters exactly once">::
-     ae [] (anagrams "patter" ["tapper"]);
-  ]
+let tests = [
+  "no matches" >::
+    ae [] (anagrams "diaper" ["hello"; "world"; "zombies"; "pants"]);
+  "detects simple anagram" >::
+    ae ["tan"] (anagrams "ant" ["tan"; "stand"; "at"]);
+  "does not detect false positives" >::
+    ae [] (anagrams "galea" ["eagle"]);
+  "detects multiple anagrams" >::
+    ae ["stream"; "maters"] (anagrams "master" ["stream"; "pigeon"; "maters"]);
+  "does not detect anagram subsets" >::
+    ae [] (anagrams "good" ["dog"; "goody"]);
+  "detects anagram" >::
+    ae ["inlets"] (anagrams "listen" ["enlists"; "google"; "inlets"; "banana"]);
+  "detects multiple anagrams" >::
+    ae ["gallery"; "regally"; "largely"] (anagrams "allergy" ["gallery"; "ballerina"; "regally"; "clergy"; "largely"; "leading"]);
+  "does not detect identical words" >::
+    ae ["cron"] (anagrams "corn" ["corn"; "dark"; "Corn"; "rank"; "CORN"; "cron"; "park"]);
+  "does not detect non-anagrams with identical checksum" >::
+    ae [] (anagrams "mass" ["last"]);
+  "detects anagrams case-insensitively" >::
+    ae ["Carthorse"] (anagrams "Orchestra" ["cashregister"; "Carthorse"; "radishes"]);
+  "detects anagrams using case-insensitive subject" >::
+    ae ["carthorse"] (anagrams "Orchestra" ["cashregister"; "carthorse"; "radishes"]);
+  "detects anagrams using case-insensitive possible matches" >::
+    ae ["Carthorse"] (anagrams "orchestra" ["cashregister"; "Carthorse"; "radishes"]);
+  "does not detect a word as its own anagram" >::
+    ae [] (anagrams "banana" ["Banana"]);
+  "does not detect a anagram if the original word is repeated" >::
+    ae [] (anagrams "go" ["go Go GO"]);
+  "anagrams must use all letters exactly once" >::
+    ae [] (anagrams "tapper" ["patter"]);
+  "capital word is not own anagram" >::
+    ae [] (anagrams "BANANA" ["Banana"]);
+]
 
 let () =
   run_test_tt_main ("anagrams tests" >::: tests)
