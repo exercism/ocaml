a9410f3a291ee52cdab4e91f042a44d097f9c6c6
Steve <stevejb71@gmail.com>
2016-11-22 22:22:39 +0800

    Sync the raindrops test with the canonical data.

--- exercises/raindrops/test.ml
+++ exercises/raindrops/test.ml
@@ -4,44 +4,43 @@ open Raindrops
 
 let ae exp got _test_ctxt = assert_equal ~printer:Fn.id exp got
 
-let tests =
-  [
-    "raindrop 1">::
-    ae "1" (raindrop 1);
-    "raindrop 3">::
-    ae "Pling" (raindrop 3);
-    "raindrop 5">::
-    ae "Plang" (raindrop 5);
-    "raindrop 7">::
-    ae "Plong" (raindrop 7);
-    "raindrop 6">::
-    ae "Pling" (raindrop 6);
-    "raindrop 8">::
-    ae "8" (raindrop 8);
-    "raindrop 9">::
-    ae "Pling" (raindrop 9);
-    "raindrop 10">::
-    ae "Plang" (raindrop 10);
-    "raindrop 14">::
-    ae "Plong" (raindrop 14);
-    "raindrop 15">::
-    ae "PlingPlang" (raindrop 15);
-    "raindrop 21">::
-    ae "PlingPlong" (raindrop 21);
-    "raindrop 25">::
-    ae "Plang" (raindrop 25);
-    "raindrop 27">::
-    ae "Pling" (raindrop 27);
-    "raindrop 35">::
-    ae "PlangPlong" (raindrop 35);
-    "raindrop 49">::
-    ae "Plong" (raindrop 49);
-    "raindrop 52">::
-    ae "52" (raindrop 52);
-    "raindrop 105">::
-    ae "PlingPlangPlong" (raindrop 105);
-    "raindrop 3125">::
-    ae "Plang" (raindrop 3125);
+let tests = [
+   "the sound for 1 is 1" >::
+      ae "1" (raindrop 1);
+   "the sound for 3 is Pling" >::
+      ae "Pling" (raindrop 3);
+   "the sound for 5 is Plang" >::
+      ae "Plang" (raindrop 5);
+   "the sound for 7 is Plong" >::
+      ae "Plong" (raindrop 7);
+   "the sound for 6 is Pling as it has a factor 3" >::
+      ae "Pling" (raindrop 6);
+   "the sound for 8 is 8" >::
+      ae "8" (raindrop 8);
+   "the sound for 9 is Pling as it has a factor 3" >::
+      ae "Pling" (raindrop 9);
+   "the sound for 10 is Plang as it has a factor 5" >::
+      ae "Plang" (raindrop 10);
+   "the sound for 14 is Plong" >::
+      ae "Plong" (raindrop 14);
+   "the sound for 15 is PlingPlang as it has factors 3 and 5" >::
+      ae "PlingPlang" (raindrop 15);
+   "the sound for 21 is PlingPlong as it has factors 3 and 7" >::
+      ae "PlingPlong" (raindrop 21);
+   "the sound for 25 is Plang as it has a factor 5" >::
+      ae "Plang" (raindrop 25);
+   "the sound for 27 is Pling as it has a factor 3" >::
+      ae "Pling" (raindrop 27);
+   "the sound for 35 is PlangPlong as it has factors 5 and 7" >::
+      ae "PlangPlong" (raindrop 35);
+   "the sound for 49 is Plong as it has a factor 7" >::
+      ae "Plong" (raindrop 49);
+   "the sound for 52 is 52" >::
+      ae "52" (raindrop 52);
+   "the sound for 105 is PlingPlangPlong as it has factors 3, 5 and 7" >::
+      ae "PlingPlangPlong" (raindrop 105);
+   "the sound for 3125 is Plang as it has a factor 5" >::
+      ae "Plang" (raindrop 3125);
 ]
 
 let () =
