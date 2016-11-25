720fd7d2998ab5fe7a3b2c92b3986a8d5bb4e5f8
Steve <stevejb71@gmail.com>
2016-11-25 23:28:42 +0800

    Use test generator to create HelloWorld test case.

--- exercises/hello-world/test.ml
+++ exercises/hello-world/test.ml
@@ -4,14 +4,11 @@ open Hello_world
 
 let ae exp got _test_ctxt = assert_equal ~printer:String.to_string exp got
 
-let tests =
-  ["no name">::
-   ae "Hello, World!" (greet None);
-   "sample name">::
-   ae "Hello, Alice!" (greet (Some "Alice"));
-   "other sample name">::
-   ae "Hello, Bob!" (greet (Some "Bob"));
-  ]
+let tests = [
+     "no name" >:: ae "Hello, World!" (greet None);
+     "sample name" >:: ae "Hello, Alice!" (greet (Some "Alice"));
+     "other sample name" >:: ae "Hello, Bob!" (greet (Some "Bob"));
+]
 
 let () =
   run_test_tt_main ("Hello World tests" >::: tests)
