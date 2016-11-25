f051510fc14a705eaf7685d4eea81d26e78e3b45
Steve <stevejb71@gmail.com>
2016-11-25 22:09:57 +0800

    Add hello-world template

---
+++ tools/test-generator/templates/hello-world/template.ml
@@ -0,0 +1,14 @@
+open Core.Std
+open OUnit2
+open Hello_world
+
+let ae exp got _test_ctxt = assert_equal ~printer:String.to_string exp got
+
+let tests = [
+  (* GENERATED-CODE
+     "$name" >:: ae $expected (greet $name);
+     END GENERATED-CODE *)
+]
+
+let () =
+  run_test_tt_main ("Hello World tests" >::: tests)
