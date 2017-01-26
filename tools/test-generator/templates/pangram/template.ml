8c3f7a5cbe9437f003bec743a06c35d6e79210fe
Steve <stevejb71@gmail.com>
2017-01-26 11:41:33 +0800

    Add test generator for the Pangram exercise

---
+++ tools/test-generator/templates/pangram/template.ml
@@ -0,0 +1,15 @@
+open Core.Std
+open OUnit2
+open Pangram
+
+let ae exp got _test_ctxt = assert_equal ~printer:Bool.to_string exp got
+
+let tests = [
+(* TEST
+   "$description" >::
+      ae $expected (is_pangram "$input");
+   END TEST *)
+]
+
+let () =
+  run_test_tt_main ("pangram tests" >::: tests)
