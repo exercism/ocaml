5cae9b88ae7d483939db16aa81387a40101c7f6a
Stephen Bastians <stevejb71@users.noreply.github.com>
2017-01-28 20:59:32 +0800

    Add pangram (#142)
    * Add test generator for the Pangram exercise
    
    * Add pangram to the config.json
    
    * Add Pangram exercise

---
+++ exercises/pangram/example.ml
@@ -0,0 +1,9 @@
+open Core.Std
+
+let string_to_set s = Char.Set.of_list (String.to_list s)
+
+let alphabet_set = string_to_set "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+let is_pangram s = 
+  let normalize s = String.filter ~f:Char.is_alpha s |> String.uppercase in
+  Set.equal alphabet_set (string_to_set @@ normalize s)
---
+++ exercises/pangram/pangram.mli
@@ -0,0 +1,1 @@
+val is_pangram : string -> bool
