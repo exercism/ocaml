a101b4d29be95040c9c2829ce01826392122d456
steve <stevejb71@gmail.com>
2017-02-03 23:57:53 +0800

    Remove .mli files.
    They were not being used due to a misunderstanding of how
    ocamlbuild works.
    It's a very small codebase with little need for encapsulation,
    so at this stage they are not really necessary anyway.

--- tools/test-generator/interfaces/special_cases.mli
+++
@@ -1,7 +0,0 @@
-open Core.Std
-
-open Model
-
-val edit_expected : stringify:(parameter -> string) -> slug:string -> key:string -> value:parameter -> string
-
-val edit_parameters : slug: string -> (string * string) list -> (string * string) list
