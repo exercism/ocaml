545a8ba5957ce3eb7a75675f654e3991284514fe
steve <stevejb71@gmail.com>
2017-02-19 22:10:51 +0800

    Update phone-number test & example solution to match canonical data.

--- exercises/phone-number/example.ml
+++ exercises/phone-number/example.ml
@@ -1,11 +1,14 @@
 open Core.Std
 
 let number s =
-  let s' = String.filter ~f:Char.is_digit s in
-  match String.length s' with
-  | 10                            -> Some s'
-  | 11 when String.get s' 0 = '1' -> Some (String.drop_prefix s' 1)
-  | _                             -> None
+  if not @@ String.is_empty (String.filter ~f:(Char.is_alpha) s)
+  then None
+  else
+    let s = String.filter ~f:(Char.is_digit) s in
+    match String.length s with
+    | 10                            -> Some s
+    | 11 when String.get s 0 = '1'  -> Some (String.drop_prefix s 1)
+    | _                             -> None
 
 let split_number s =
   (String.slice s 0 3, String.slice s 3 6, String.slice s 6 10)
