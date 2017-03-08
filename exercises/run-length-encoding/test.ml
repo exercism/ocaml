0f299446c8b26f8d4cc53ee7b09f5239de384a09
steve <stevejb71@gmail.com>
2017-03-08 21:26:52 +0800

    Update run length encoding test to match new canonical data.

--- exercises/run-length-encoding/test.ml
+++ exercises/run-length-encoding/test.ml
@@ -13,6 +13,10 @@ let encode_tests = [
      ae "2A3B4C" (encode "AABBBCCCC");
    "single characters mixed with repeated characters" >::
      ae "12WB12W3B24WB" (encode "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB");
+   "multiple whitespace mixed in string" >::
+     ae "2 hs2q q2w2 " (encode "  hsqq qww  ");
+   "lowercase characters" >::
+     ae "2a3b4c" (encode "aabbbcccc");
 ]
 
 let decode_tests = [
@@ -24,11 +28,15 @@ let decode_tests = [
      ae "AABBBCCCC" (decode "2A3B4C");
    "single characters with repeated characters" >::
      ae "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB" (decode "12WB12W3B24WB");
+   "multiple whitespace mixed in string" >::
+     ae "  hsqq qww  " (decode "2 hs2q q2w2 ");
+   "lower case string" >::
+     ae "aabbbcccc" (decode "2a3b4c");
 ]
 
 let consistency_tests = [
    "encode followed by decode gives original string" >::
-     ae "zzz ZZ  zZ" (decode (encode "zzz ZZ  zZ"));
+     ae "zzz ZZ  zZ" (decode "zzz ZZ  zZ");
 ]
 
 let () =
