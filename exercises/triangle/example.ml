a2780bf999b1e5e35eb0a3dc3b36bc0b81bde8fa
steve <stevejb71@gmail.com>
2017-03-19 16:55:56 +0800

    Take list out of triangle interfaces.

--- exercises/triangle/example.ml
+++ exercises/triangle/example.ml
@@ -1,17 +1,15 @@
 open Core.Std
 
-let sort_sides = function
-| [_; _; _] as sides ->
-    let side = List.nth_exn (List.sort sides ~cmp:Int.compare) in
-    (side 0, side 1, side 2)
-| _ -> failwith "not at triangle"
+let sort_sides a b c = 
+  let side = List.nth_exn (List.sort ~cmp:Int.compare [a; b; c]) in
+  (side 0, side 1, side 2)
 
-let is_triangle f sides = 
-  let (a, b, c) = sort_sides sides in
-  c > 0 && c <= a + b && f a b c
+let is_triangle a b c = 
+  let (a, b, c) = sort_sides a b c in
+  c > 0 && c <= a + b
 
-let is_equilateral = is_triangle (fun a b c -> a = b && b = c)
+let is_equilateral a b c = is_triangle a b c && a = b && b = c
 
-let is_isoceles = is_triangle (fun a b c -> a = b || b = c)
+let is_isoceles a b c = is_triangle a b c && (a = b || b = c || a = c)
 
-let is_scalene = is_triangle (fun a b c -> a <> b && b <> c)
+let is_scalene a b c = is_triangle a b c && (a <> b && b <> c)
