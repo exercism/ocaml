e1d9da0b7765772b9455b1f7b65cf650ea4d5b86
steve <stevejb71@gmail.com>
2017-05-03 00:25:44 +0800

    Fix typo in triangle and use floats
    is_isosceles was spelt wrongly.
    Canonical data now uses floats for sides, so changing
    to match that.

--- exercises/triangle/example.ml
+++ exercises/triangle/example.ml
@@ -1,15 +1,15 @@
 open Core.Std
 
 let sort_sides a b c = 
-  let side = List.nth_exn (List.sort ~cmp:Int.compare [a; b; c]) in
+  let side = List.nth_exn (List.sort ~cmp:Float.compare [a; b; c]) in
   (side 0, side 1, side 2)
 
 let is_triangle a b c = 
   let (a, b, c) = sort_sides a b c in
-  c > 0 && c <= a + b
+  c > 0. && c <= a +. b
 
 let is_equilateral a b c = is_triangle a b c && a = b && b = c
 
-let is_isoceles a b c = is_triangle a b c && (a = b || b = c || a = c)
+let is_isosceles a b c = is_triangle a b c && (a = b || b = c || a = c)
 
 let is_scalene a b c = is_triangle a b c && (a <> b && b <> c)
