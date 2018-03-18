c16ea87dc1554a6f32d4a3e3b57982ef6f02fa96
steve <stevejb71@gmail.com>
2018-03-18 12:57:06 +0800

    Remove comment in space age test

--- exercises/space-age/test.ml
+++ exercises/space-age/test.ml
@@ -4,7 +4,6 @@ open Base
 open OUnit2
 open Space_age
 
-(* Assert In Delta *)
 let ae ~delta:delta exp got _ctxt =
   let msg = Printf.sprintf "Expected %f got %f, difference is greater than %f"
                     exp got delta in
