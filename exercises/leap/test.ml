a87a66b1f8f5ac70821dc2827bd4a034890b9486
Damián Rotta <damian_rott2@hotmail.com>
2018-03-20 08:53:24 -0300

    Base.string_of_bool <- Bool.to_string
    Base.string_of_bool is deprecated since 2016-09 and produce a warning.

--- exercises/leap/test.ml
+++ exercises/leap/test.ml
@@ -3,7 +3,7 @@
 open OUnit2
 open Leap
 
-let ae exp got _test_ctxt = assert_equal exp got ~printer:string_of_bool
+let ae exp got _test_ctxt = assert_equal exp got ~printer:Bool.to_string
 
 let tests = [
   "year not divisible by 4: common year" >::
