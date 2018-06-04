262ad8bba0b46917ab02fe3265f6be626d98a43b
steve <stevejb71@gmail.com>
2018-06-04 21:50:03 +0800

    Sync some tests from problem specs

--- exercises/luhn/example.ml
+++ exercises/luhn/example.ml
@@ -8,9 +8,12 @@ let valid s =
   let s = String.filter s ~f:(fun ch -> Char.(ch <> ' ')) in
   if String.length s > 1
   then
-    let checksum = String.to_list s
-    |> List.rev_map ~f:(fun ch -> Char.to_int ch - Char.to_int '0')
-    |> every_second_digit_doubled
-    |> List.sum (module Int) ~f:Fn.id in
-    checksum % 10 = 0
+    if String.exists s ~f:(Fn.non Char.is_digit)
+    then false
+    else
+      let checksum = String.to_list s
+      |> List.rev_map ~f:(fun ch -> Char.to_int ch - Char.to_int '0')
+      |> every_second_digit_doubled
+      |> List.sum (module Int) ~f:Fn.id in
+      checksum % 10 = 0
   else false
