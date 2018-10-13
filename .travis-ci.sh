a869c5c6d6790c826e91df4d3b10000ed7c56fbd
Steve Bastians <stevejb71@gmail.com>
2018-10-13 09:23:45 +0800

    Upgrade OPAM

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,15 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind core ounit qcheck react ppx_deriving"
 
-echo "yes" | sudo add-apt-repository ppa:avsm/ocaml42+opam12
+echo "Downloading OPAM install"
+wget https://github.com/ocaml/opam/releases/download/2.0.0/opam-full-2.0.0.tar.gz
+echo "Unzipping OPAM package"
+gzip -d opam-full-2.0.0.tar.gz
+tar -xf opam-full-2.0.0.tar 
+echo "WHERE ARE WE"
+ls
+pwd
+
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
@@ -10,7 +18,6 @@ echo OCaml version
 ocaml -version
 echo OPAM versions
 opam --version
-opam --git-version
 
 opam init
 opam switch $OCAML_VERSION
