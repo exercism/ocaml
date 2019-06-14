1a9dd73fa34af889996d974b160ac737d6729dc2
Mario Nebl <hello+github.com@mario-nebl.de>
2019-06-14 20:03:31 +0000

    Undo typo

--- exercises/bowling/bowling.mli
+++ exercises/bowling/bowling.mli
@@ -10,5 +10,5 @@ val new_game: t
 of pins knocked down. The return value is the updated state of the game. *)
 val roll : int -> t -> (t, string) Result.t
 
-(** This is called at the end of a game to retrieve the finacd l score. *)
+(** This is called at the end of a game to retrieve the final score. *)
 val score : t -> (int, string) Result.t
