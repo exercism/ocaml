71122555f1f79046331cfea570e48e137e6228b1
Mario Nebl <hello+github.com@mario-nebl.de>
2019-06-19 17:58:40 +0000

    use ocaml/opam2 docker for ci

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,25 +1,4 @@
-# Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind core ounit qcheck react ppx_deriving"
-
-case "$OCAML_VERSION,$OPAM_VERSION" in
-    4.07.0,1.2.2) ppa=avsm/ocaml42+opam12 ;;
-    *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
-esac
-
-echo "yes" | sudo add-apt-repository ppa:$ppa
-sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-export OPAMYES=1
-export OPAMVERBOSE=1
-echo OCaml version
-ocaml -version
-echo OPAM versions
-opam --version
-opam --git-version
-
-opam init
-opam switch $OCAML_VERSION
-eval `opam config env`
-opam install ${OPAM_DEPENDS}
-
+sudo apk add m4 linux-headers
+opam install dune ocamlfind core ounit qcheck react ppx_deriving 
+eval $(opam env)
 make test
