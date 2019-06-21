4a10681af65024bb15d09020943c5f62e2226b26
Mario Nebl <marionebl@users.noreply.github.com>
2019-06-21 11:40:03 +0200

    fixup travis generate function

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,3 +1,5 @@
+set -e
+
 if [ $1 == "test" ]; then
   sudo apk add m4 linux-headers
   opam pin add base v0.11.1
@@ -11,16 +13,14 @@ if [ $1 == "generate" ]; then
   opam pin add jbuilder 1.0+beta20
   opam install core yojson ounit ocp-indent
   eval $(opam env)
-  sudo make -C tools/test-generator
   sudo git clone https://github.com/exercism/problem-specifications.git ../problem-specifications
   cd ../problem-specifications
   sudo git checkout 2af3c9b0074f16c62366c5c533eaacd3ff27b583 
   cd -
   cd tools/test-generator
-  sudo ./test_gen.exe 
-  ocp-indent -i ../../exercises/**/test.ml
+  sudo dune exec ./test_gen.exe --profile=release 
   cd -
-  ocp-indent -i exercises/**/test.ml
+  sudo ocp-indent -i ../../exercises/**/test.ml
   if output=$(git status --porcelain -- "exercises/**/test.ml") && [ -z "$output" ]; then
     echo "Tests are in sync."
   else
