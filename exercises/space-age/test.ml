321a380e74a4a00abb02890ed45a54c8f75b05b8
Mario Nebl <hello+github.com@mario-nebl.de>
2019-06-22 10:24:43 +0000

    space-age: adapt to test data

--- exercises/space-age/test.ml
+++ exercises/space-age/test.ml
@@ -4,26 +4,26 @@ open Space_age
 
 let ae ~delta:delta exp got _ctxt =
   let msg = Printf.sprintf "Expected %f got %f, difference is greater than %f"
-      exp got delta in
+                    exp got delta in
   assert_bool msg (cmp_float ~epsilon:delta exp got)
 
 let tests = [
-  "age on Earth" >::
-  ae ~delta:0.005 31.69 (age_on Earth 1000000000);
-  "age on Mercury" >::
-  ae ~delta:0.005 280.88 (age_on Mercury 2134835688);
-  "age on Venus" >::
-  ae ~delta:0.005 9.78 (age_on Venus 189839836);
-  "age on Mars" >::
-  ae ~delta:0.005 35.88 (age_on Mars 2129871239);
-  "age on Jupiter" >::
-  ae ~delta:0.005 2.41 (age_on Jupiter 901876382);
-  "age on Saturn" >::
-  ae ~delta:0.005 2.15 (age_on Saturn 2000000000);
-  "age on Uranus" >::
-  ae ~delta:0.005 0.46 (age_on Uranus 1210123456);
-  "age on Neptune" >::
-  ae ~delta:0.005 0.35 (age_on Neptune 1821023456);
+   "age on Earth" >::
+      ae ~delta:0.05 31.69 (age_on Earth 1000000000);
+   "age on Mercury" >::
+      ae ~delta:0.05 280.88 (age_on Mercury 2134835688);
+   "age on Venus" >::
+      ae ~delta:0.05 9.78 (age_on Venus 189839836);
+   "age on Mars" >::
+      ae ~delta:0.05 35.88 (age_on Mars 2129871239);
+   "age on Jupiter" >::
+      ae ~delta:0.05 2.41 (age_on Jupiter 901876382);
+   "age on Saturn" >::
+      ae ~delta:0.05 2.15 (age_on Saturn 2000000000);
+   "age on Uranus" >::
+      ae ~delta:0.05 0.46 (age_on Uranus 1210123456);
+   "age on Neptune" >::
+      ae ~delta:0.05 0.35 (age_on Neptune 1821023456);
 ]
 
 let () =
