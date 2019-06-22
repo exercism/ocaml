6edc40a3f2a2b779b599d05cedb40f0671fcedff
Mario Nebl <hello+github.com@mario-nebl.de>
2019-06-22 08:09:20 +0000

    forth: update test file

--- exercises/forth/test.ml
+++ exercises/forth/test.ml
@@ -9,141 +9,141 @@ let ae exp got _test_ctxt = assert_equal ~printer:print_int_list_option exp got
 
 let parsing_and_numbers_tests = [
   "numbers just get pushed onto the stack" >::
-  ae (Ok [1; 2; 3; 4; 5]) (evaluate ["1 2 3 4 5"]);
+  ae (Some [1; 2; 3; 4; 5]) (evaluate ["1 2 3 4 5"]);
 ]
 
 
 let addition_tests = [
   "can add two numbers" >::
-  ae (Ok [3]) (evaluate ["1 2 +"]);
+  ae (Some [3]) (evaluate ["1 2 +"]);
   "errors if there is nothing on the stack" >::
-  ae (Error "empty stack") (evaluate ["+"]);
+  ae None (evaluate ["+"]);
   "errors if there is only one value on the stack" >::
-  ae (Error "only one value on the stack") (evaluate ["1 +"]);
+  ae None (evaluate ["1 +"]);
 ]
 
 
 let subtraction_tests = [
   "can subtract two numbers" >::
-  ae (Ok [-1]) (evaluate ["3 4 -"]);
+  ae (Some [-1]) (evaluate ["3 4 -"]);
   "errors if there is nothing on the stack" >::
-  ae (Error "empty stack") (evaluate ["-"]);
+  ae None (evaluate ["-"]);
   "errors if there is only one value on the stack" >::
-  ae (Error "only one value on the stack") (evaluate ["1 -"]);
+  ae None (evaluate ["1 -"]);
 ]
 
 
 let multiplication_tests = [
   "can multiply two numbers" >::
-  ae (Ok [8]) (evaluate ["2 4 *"]);
+  ae (Some [8]) (evaluate ["2 4 *"]);
   "errors if there is nothing on the stack" >::
-  ae (Error "empty stack") (evaluate ["*"]);
+  ae None (evaluate ["*"]);
   "errors if there is only one value on the stack" >::
-  ae (Error "only one value on the stack") (evaluate ["1 *"]);
+  ae None (evaluate ["1 *"]);
 ]
 
 
 let division_tests = [
   "can divide two numbers" >::
-  ae (Ok [4]) (evaluate ["12 3 /"]);
+  ae (Some [4]) (evaluate ["12 3 /"]);
   "performs integer division" >::
-  ae (Ok [2]) (evaluate ["8 3 /"]);
+  ae (Some [2]) (evaluate ["8 3 /"]);
   "errors if dividing by zero" >::
-  ae (Error "divide by zero") (evaluate ["4 0 /"]);
+  ae None (evaluate ["4 0 /"]);
   "errors if there is nothing on the stack" >::
-  ae (Error "empty stack") (evaluate ["/"]);
+  ae None (evaluate ["/"]);
   "errors if there is only one value on the stack" >::
-  ae (Error "only one value on the stack") (evaluate ["1 /"]);
+  ae None (evaluate ["1 /"]);
 ]
 
 
 let combined_arithmetic_tests = [
   "addition and subtraction" >::
-  ae (Ok [-1]) (evaluate ["1 2 + 4 -"]);
+  ae (Some [-1]) (evaluate ["1 2 + 4 -"]);
   "multiplication and division" >::
-  ae (Ok [2]) (evaluate ["2 4 * 3 /"]);
+  ae (Some [2]) (evaluate ["2 4 * 3 /"]);
 ]
 
 
 let dup_tests = [
   "copies a value on the stack" >::
-  ae (Ok [1; 1]) (evaluate ["1 dup"]);
+  ae (Some [1; 1]) (evaluate ["1 dup"]);
   "copies the top value on the stack" >::
-  ae (Ok [1; 2; 2]) (evaluate ["1 2 dup"]);
+  ae (Some [1; 2; 2]) (evaluate ["1 2 dup"]);
   "errors if there is nothing on the stack" >::
-  ae (Error "empty stack") (evaluate ["dup"]);
+  ae None (evaluate ["dup"]);
 ]
 
 
 let drop_tests = [
   "removes the top value on the stack if it is the only one" >::
-  ae (Ok []) (evaluate ["1 drop"]);
+  ae (Some []) (evaluate ["1 drop"]);
   "removes the top value on the stack if it is not the only one" >::
-  ae (Ok [1]) (evaluate ["1 2 drop"]);
+  ae (Some [1]) (evaluate ["1 2 drop"]);
   "errors if there is nothing on the stack" >::
-  ae (Error "empty stack") (evaluate ["drop"]);
+  ae None (evaluate ["drop"]);
 ]
 
 
 let swap_tests = [
   "swaps the top two values on the stack if they are the only ones" >::
-  ae (Ok [2; 1]) (evaluate ["1 2 swap"]);
+  ae (Some [2; 1]) (evaluate ["1 2 swap"]);
   "swaps the top two values on the stack if they are not the only ones" >::
-  ae (Ok [1; 3; 2]) (evaluate ["1 2 3 swap"]);
+  ae (Some [1; 3; 2]) (evaluate ["1 2 3 swap"]);
   "errors if there is nothing on the stack" >::
-  ae (Error "empty stack") (evaluate ["swap"]);
+  ae None (evaluate ["swap"]);
   "errors if there is only one value on the stack" >::
-  ae (Error "only one value on the stack") (evaluate ["1 swap"]);
+  ae None (evaluate ["1 swap"]);
 ]
 
 
 let over_tests = [
   "copies the second element if there are only two" >::
-  ae (Ok [1; 2; 1]) (evaluate ["1 2 over"]);
+  ae (Some [1; 2; 1]) (evaluate ["1 2 over"]);
   "copies the second element if there are more than two" >::
-  ae (Ok [1; 2; 3; 2]) (evaluate ["1 2 3 over"]);
+  ae (Some [1; 2; 3; 2]) (evaluate ["1 2 3 over"]);
   "errors if there is nothing on the stack" >::
-  ae (Error "empty stack") (evaluate ["over"]);
+  ae None (evaluate ["over"]);
   "errors if there is only one value on the stack" >::
-  ae (Error "only one value on the stack") (evaluate ["1 over"]);
+  ae None (evaluate ["1 over"]);
 ]
 
 
 let user_defined_words_tests = [
   "can consist of built-in words" >::
-  ae (Ok [1; 1; 1]) (evaluate [": dup-twice dup dup ;"; "1 dup-twice"]);
+  ae (Some [1; 1; 1]) (evaluate [": dup-twice dup dup ;"; "1 dup-twice"]);
   "execute in the right order" >::
-  ae (Ok [1; 2; 3]) (evaluate [": countup 1 2 3 ;"; "countup"]);
+  ae (Some [1; 2; 3]) (evaluate [": countup 1 2 3 ;"; "countup"]);
   "can override other user-defined words" >::
-  ae (Ok [1; 1; 1]) (evaluate [": foo dup ;"; ": foo dup dup ;"; "1 foo"]);
+  ae (Some [1; 1; 1]) (evaluate [": foo dup ;"; ": foo dup dup ;"; "1 foo"]);
   "can override built-in words" >::
-  ae (Ok [1; 1]) (evaluate [": swap dup ;"; "1 swap"]);
+  ae (Some [1; 1]) (evaluate [": swap dup ;"; "1 swap"]);
   "can override built-in operators" >::
-  ae (Ok [12]) (evaluate [": + * ;"; "3 4 +"]);
+  ae (Some [12]) (evaluate [": + * ;"; "3 4 +"]);
   "can use different words with the same name" >::
-  ae (Ok [5; 6]) (evaluate [": foo 5 ;"; ": bar foo ;"; ": foo 6 ;"; "bar foo"]);
+  ae (Some [5; 6]) (evaluate [": foo 5 ;"; ": bar foo ;"; ": foo 6 ;"; "bar foo"]);
   "can define word that uses word with the same name" >::
-  ae (Ok [11]) (evaluate [": foo 10 ;"; ": foo foo 1 + ;"; "foo"]);
+  ae (Some [11]) (evaluate [": foo 10 ;"; ": foo foo 1 + ;"; "foo"]);
   "cannot redefine numbers" >::
-  ae (Error "illegal operation") (evaluate [": 1 2 ;"]);
+  ae None (evaluate [": 1 2 ;"]);
   "errors if executing a non-existent word" >::
-  ae (Error "undefined operation") (evaluate ["foo"]);
+  ae None (evaluate ["foo"]);
 ]
 
 
 let case_insensitivity = [
   "DUP is case-insensitive" >::
-  ae (Ok [1; 1; 1; 1]) (evaluate ["1 DUP Dup dup"]);
+  ae (Some [1; 1; 1; 1]) (evaluate ["1 DUP Dup dup"]);
   "DROP is case-insensitive" >::
-  ae (Ok [1]) (evaluate ["1 2 3 4 DROP Drop drop"]);
+  ae (Some [1]) (evaluate ["1 2 3 4 DROP Drop drop"]);
   "SWAP is case-insensitive" >::
-  ae (Ok [2; 3; 4; 1]) (evaluate ["1 2 SWAP 3 Swap 4 swap"]);
+  ae (Some [2; 3; 4; 1]) (evaluate ["1 2 SWAP 3 Swap 4 swap"]);
   "OVER is case-insensitive" >::
-  ae (Ok [1; 2; 1; 2; 1]) (evaluate ["1 2 OVER Over over"]);
+  ae (Some [1; 2; 1; 2; 1]) (evaluate ["1 2 OVER Over over"]);
   "user-defined words are case-insensitive" >::
-  ae (Ok [1; 1; 1; 1]) (evaluate [": foo dup ;"; "1 FOO Foo foo"]);
+  ae (Some [1; 1; 1; 1]) (evaluate [": foo dup ;"; "1 FOO Foo foo"]);
   "definitions are case-insensitive" >::
-  ae (Ok [1; 1; 1; 1]) (evaluate [": SWAP DUP Dup dup ;"; "1 swap"]);
+  ae (Some [1; 1; 1; 1]) (evaluate [": SWAP DUP Dup dup ;"; "1 swap"]);
 ]
 
 let () =
