d1c0544cbc0d33de73aa82279b85b3020efe7edd
Mario Nebl <hello+github.com@mario-nebl.de>
2019-06-25 13:59:35 +0000

    palindrome-products: adapt to optional interface

--- .travis/.travis-ci.sh
+++ .travis/.travis-ci.sh
@@ -32,7 +32,7 @@ sudo git checkout -- exercises/hello-world/test.ml
 # sudo git checkout -- exercises/luhn/test.ml
 # sudo git checkout -- exercises/leap/test.ml
 # sudo git checkout -- exercises/minesweeper/test.ml
-sudo git checkout -- exercises/palindrome-products/test.ml
+# sudo git checkout -- exercises/palindrome-products/test.ml
 sudo git checkout -- exercises/pangram/test.ml
 sudo git checkout -- exercises/phone-number/test.ml
 sudo git checkout -- exercises/prime-factors/test.ml
--- exercises/palindrome-products/test.ml
+++ exercises/palindrome-products/test.ml
@@ -15,40 +15,40 @@ let ae exp got _test_ctxt =
 
 let tests = [
    "finds the smallest palindrome from single digit factors" >::
-      ae (Ok {value=1; factors=[(1,1)]})
+      ae (Ok {value=(Some 1); factors=[(1,1)]})
          (smallest ~min:1 ~max:9);
    "finds the largest palindrome from single digit factors" >::
-      ae (Ok {value=9; factors=[(1,9); (3,3)]})
+      ae (Ok {value=(Some 9); factors=[(1,9); (3,3)]})
          (largest ~min:1 ~max:9);
    "find the smallest palindrome from double digit factors" >::
-      ae (Ok {value=121; factors=[(11,11)]})
+      ae (Ok {value=(Some 121); factors=[(11,11)]})
          (smallest ~min:10 ~max:99);
    "find the largest palindrome from double digit factors" >::
-      ae (Ok {value=9009; factors=[(91,99)]})
+      ae (Ok {value=(Some 9009); factors=[(91,99)]})
          (largest ~min:10 ~max:99);
    "find smallest palindrome from triple digit factors" >::
-      ae (Ok {value=10201; factors=[(101,101)]})
+      ae (Ok {value=(Some 10201); factors=[(101,101)]})
          (smallest ~min:100 ~max:999);
    "find the largest palindrome from triple digit factors" >::
-      ae (Ok {value=906609; factors=[(913,993)]})
+      ae (Ok {value=(Some 906609); factors=[(913,993)]})
          (largest ~min:100 ~max:999);
    "find smallest palindrome from four digit factors" >::
-      ae (Ok {value=1002001; factors=[(1001,1001)]})
+      ae (Ok {value=(Some 1002001); factors=[(1001,1001)]})
          (smallest ~min:1000 ~max:9999);
    "find the largest palindrome from four digit factors" >::
-      ae (Ok {value=99000099; factors=[(9901,9999)]})
+      ae (Ok {value=(Some 99000099); factors=[(9901,9999)]})
          (largest ~min:1000 ~max:9999);
    "empty result for smallest if no palindrome in the range" >::
-      ae (Error "no palindrome with factors in the range 1002 to 1003")
+      ae (Ok {value=None; factors=[]})
          (smallest ~min:1002 ~max:1003);
    "empty result for largest if no palindrome in the range" >::
-      ae (Error "no palindrome with factors in the range 15 to 15")
+      ae (Ok {value=None; factors=[]})
          (largest ~min:15 ~max:15);
    "error result for smallest if min is more than max" >::
-      ae (Error "invalid input: min is 10000 and max is 1")
+      ae (Error "min must be <= max")
          (smallest ~min:10000 ~max:1);
    "error result for largest if min is more than max" >::
-      ae (Error "invalid input: min is 2 and max is 1")
+      ae (Error "min must be <= max")
          (largest ~min:2 ~max:1);
 ]
 
