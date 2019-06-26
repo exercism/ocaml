11870ef984534d36f4d8eac8a90dc315e87ccdda
Mario Nebl <hello+github.com@mario-nebl.de>
2019-06-26 20:27:41 +0000

    test-generator: port bins to base

--- tools/test-generator/bin_test_gen/test_gen.ml
+++ tools/test-generator/bin_test_gen/test_gen.ml
@@ -1,42 +1,43 @@
-open Core
+open Base
 open Generator
-open Generator.Languages
 
-let is_directory =
-  Command.Spec.Arg_type.create
-    (fun n ->
-       match Sys.is_directory n with
-       | `Yes -> n
-       | `No | `Unknown ->
-         eprintf "'%s' is not a regular folder.\n%!" n;
-         exit 1
-    )
+let home_dir = Unix.getenv "HOME"
 
-let home_dir = Option.value_exn (Sys.getenv "HOME")
+let language_value = ref None
+let template_value = ref None
+let data_folder_value = ref None
+let output_folder_value = ref None
+let generated_folder_value = ref None
+let filter_value = ref None
 
-let default_generated name = Option.value ~default:(home_dir ^ "/.exercism-" ^ name ^ "-generated")
-
-let command =
-  let open Command.Let_syntax in
-  Command.basic
-    ~summary:"Generates test code from canonical data."
-    ~readme: (fun () -> "Generates test code from canonical data.")
-    [%map_open
-      let language = flag "l" (optional string) ~doc:"language to generate tests for"
-      and templates_folder = flag "t" (optional_with_default "../templates" is_directory) ~doc:"string Directory containing templates."
-      and canonical_data_folder = flag "c" (optional_with_default "../../../../problem-specifications/exercises" is_directory) ~doc:"string Directory containing canonical data."
-      and output_folder = flag "-o" (optional string) ~doc:"string Directory to output generated tests."
-      and generated_folder = flag "-g" (optional string) ~doc:"string Directory to backup generated tests."
-      and filter = flag "-f" (optional string) ~doc:"string Filter out files not matching this string."
-      in
-      fun () ->
-        let language = Option.value language ~default:"ocaml" in
-        let lc = default_language_config language in
-        let generated_folder = default_generated language generated_folder in
-        let templates_folder = templates_folder ^ "/" ^ language in
-        let output_folder = Option.value output_folder ~default:(lc.default_base_folder ^ "/../exercises") in
-        Controller.run lc templates_folder canonical_data_folder output_folder generated_folder filter
-    ]
+let spec = Getopts.spec 
+  "[-l string]"
+  "Generates test code from canonical data."
+  [
+    Getopts.string 'l' (fun l _ -> language_value := Some l) "language to generate tests for";
+    Getopts.string 't' (fun t _ -> template_value := Some t) "directory containing templates";
+    Getopts.string 'c' (fun c _ -> data_folder_value := Some c) "directory containing templates";
+    Getopts.string 'o' (fun c _ -> output_folder_value := Some c) "directory to output generated tests";
+    Getopts.string 'o' (fun c _ -> generated_folder_value := Some c) "directory to backup generated tests";
+    Getopts.string 'f' (fun c _ -> filter_value := Some c) "filter out files not matching this string";
+  ] 
+  (fun _ _ -> ())
+  []
 
 let () =
-  Command.run ~version:"0.1" command
+  Getopts.parse_argv spec ();
+  let language = !language_value |> Option.value ~default:"ocaml" in
+  let language_config = Languages.default_language_config language in
+  let templates_folder = !template_value |> Option.value ~default:"../templates" in
+  let templates_folder = templates_folder ^ "/" ^ language in
+  let canonical_data_folder = !data_folder_value |> Option.value ~default:"../../../../problem-specifications/exercises" in
+  let output_folder = !output_folder_value |> Option.value ~default:(language_config.default_base_folder ^ "/../exercises") in
+  let generated_folder = !generated_folder_value |> Option.value ~default:(home_dir ^ "/.exercism-" ^ language ^ "-generated") in
+  let filter = !filter_value in
+  Controller.run 
+    ~language_config 
+    ~templates_folder
+    ~canonical_data_folder 
+    ~output_folder 
+    ~generated_folder 
+    filter;
