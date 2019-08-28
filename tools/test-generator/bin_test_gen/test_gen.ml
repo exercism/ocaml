d3cae426be1a764ea4123d4c9fc4766bcdfc6ca7
Mario Nebl <hello+github.com@mario-nebl.de>
2019-08-28 18:52:21 +0000

    test-generator: remove unused purescript generator code

--- tools/test-generator/bin_test_gen/test_gen.ml
+++ tools/test-generator/bin_test_gen/test_gen.ml
@@ -1,9 +1,9 @@
 open Base
 open Generator
+open Yojson.Basic
 
 let home_dir = Unix.getenv "HOME"
 
-let language_value = ref None
 let template_value = ref None
 let data_folder_value = ref None
 let output_folder_value = ref None
@@ -14,7 +14,6 @@ let spec = Getopts.spec
   "[-l string]"
   "Generates test code from canonical data."
   [
-    Getopts.string 'l' (fun l _ -> language_value := Some l) "language to generate tests for";
     Getopts.string 't' (fun t _ -> template_value := Some t) "directory containing templates";
     Getopts.string 'c' (fun c _ -> data_folder_value := Some c) "directory containing templates";
     Getopts.string 'o' (fun c _ -> output_folder_value := Some c) "directory to output generated tests";
@@ -24,15 +23,28 @@ let spec = Getopts.spec
   (fun _ _ -> ())
   []
 
+type language_config = {
+  template_file_name: string;
+  default_base_folder: string;
+  test_start_marker: string;
+  test_end_marker: string;
+  edit_parameters: slug: string -> (string * json) list -> (string * string) list option;
+}
+
+let language_config = {
+  template_file_name = "test.ml"; 
+  default_base_folder = "../..";
+  test_start_marker = "(* TEST"; 
+  test_end_marker = "END TEST";
+  edit_parameters = ocaml_edit_parameters
+}
+
 let () =
   Getopts.parse_argv spec ();
-  let language = !language_value |> Option.value ~default:"ocaml" in
-  let language_config = Languages.default_language_config language in
-  let templates_folder = !template_value |> Option.value ~default:"../templates" in
-  let templates_folder = templates_folder ^ "/" ^ language in
+  let templates_folder = (!template_value |> Option.value ~default:"../templates") ^ "/" in
   let canonical_data_folder = !data_folder_value |> Option.value ~default:"../../../../problem-specifications/exercises" in
   let output_folder = !output_folder_value |> Option.value ~default:(language_config.default_base_folder ^ "/../exercises") in
-  let generated_folder = !generated_folder_value |> Option.value ~default:(home_dir ^ "/.exercism-" ^ language ^ "-generated") in
+  let generated_folder = !generated_folder_value |> Option.value ~default:(home_dir ^ "/.exercism-ocaml-generated") in
   let filter = !filter_value in
   Controller.run 
     ~language_config 
