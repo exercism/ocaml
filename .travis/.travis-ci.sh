f929d73c809faacd82b54a2739715ce3b3b916b0
Mario Nebl <hello+github.com@mario-nebl.de>
2019-09-08 07:42:35 +0000

    ci: remove obsolete ocp-indent call

--- .travis/.travis-ci.sh
+++ .travis/.travis-ci.sh
@@ -13,7 +13,6 @@ cd /repo/tools/test-generator/bin_test_gen
 dune exec ./test_gen.exe --profile=release -- -w ../../../../
 
 cd /repo
-ocp-indent -i exercises/**/test.ml
 
 if output=$(git status --porcelain -- "exercises/**/test.ml") && [ -z "$output" ]; then
  echo "Tests are in sync."
