8de33e4ccae346dc2f106c7493fdc96c2cf095e5
Emmanuel Delaborde <emmanuel.delaborde@modicagroup.com>
2019-10-04 14:24:38 +1300

    fix error messages

--- exercises/binary-search-tree/binary_search_tree.ml
+++ exercises/binary-search-tree/binary_search_tree.ml
@@ -4,13 +4,13 @@ type bst
 
 let empty = failwith "'empty' is missing"
 
-let value _ = failwith "'empty' is missing"
+let value _ = failwith "'value' is missing"
 
-let left _ = failwith "'empty' is missing"
+let left _ = failwith "'left' is missing"
 
-let right _ = failwith "'empty' is missing"
+let right _ = failwith "'right' is missing"
 
-let insert _ _ = failwith "'empty' is missing"
+let insert _ _ = failwith "'insert' is missing"
 
-let to_list _ = failwith "'empty' is missing"
+let to_list _ = failwith "'to_list' is missing"
 
