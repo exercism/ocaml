6ed645d26f5ef85ee3fab7073dc03e16a4980e28
Haz <hyphens@pm.me>
2020-07-23 00:30:30 +0000

    Some isn't Result.t

--- exercises/practice/say/say.mli
+++ exercises/practice/say/say.mli
@@ -2,7 +2,7 @@ open Base
 
 (* Spells out a number from 0 to 999,999,999,999 in English.
    If the input is outside that range, then an Error is returned.
-   E.g in_english 100L = Some "one hundred"
+   E.g in_english 100L = Ok "one hundred"
        in_english (-1)L = Error ""
  *)
 val in_english : int64 -> (string, string) Result.t
