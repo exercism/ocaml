ce2b087d07c717817c2f91eece452186be54e98f
George Shammas <george@shamm.as>
2023-02-15 10:51:18 -0500

    leap: Sync with problem-specfications

--- exercises/practice/leap/test.ml
+++ exercises/practice/leap/test.ml
@@ -1,4 +1,3 @@
-(* leap - 1.5.1 *)
 open OUnit2
 open Leap
 
@@ -11,10 +10,16 @@ let tests = [
   ae false (leap_year 1970);
   "year divisible by 4, not divisible by 100 in leap year" >::
   ae true (leap_year 1996);
+  "year divisible by 4 and 5 is still a leap year" >::
+  ae true (leap_year 1960);
   "year divisible by 100, not divisible by 400 in common year" >::
   ae false (leap_year 2100);
-  "year divisible by 400 in leap year" >::
+  "year divisible by 100 but not by 3 is still not a leap year" >::
+  ae false (leap_year 1900);
+  "year divisible by 400 is leap year" >::
   ae true (leap_year 2000);
+  "year divisible by 400 but not by 125 is still a leap year" >::
+  ae true (leap_year 2400);
   "year divisible by 200, not divisible by 400 in common year" >::
   ae false (leap_year 1800);
 ]
