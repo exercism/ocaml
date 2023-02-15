8caf946c89e2e9fb43abfe4564cb9e1abda7bd80
George Shammas <george@shamm.as>
2023-02-15 10:51:21 -0500

    luhn: Sync with problem-specfications

--- exercises/practice/luhn/test.ml
+++ exercises/practice/luhn/test.ml
@@ -1,4 +1,3 @@
-(* luhn - 1.6.1 *)
 open Base
 open OUnit2
 open Luhn
@@ -21,6 +20,10 @@ let tests = [
   assert_valid false "055 444 286";
   "invalid credit card" >::
   assert_valid false "8273 1232 7352 0569";
+  "invalid long number with an even remainder" >::
+  assert_valid false "1 2345 6789 1234 5678 9012";
+  "invalid long number with a remainder divisible by 5" >::
+  assert_valid false "1 2345 6789 1234 5678 9013";
   "valid number with an even number of digits" >::
   assert_valid true "095 245 88";
   "valid number with an odd number of spaces" >::
@@ -37,10 +40,16 @@ let tests = [
   assert_valid true "0000 0";
   "input digit 9 is correctly converted to output digit 9" >::
   assert_valid true "091";
+  "very long input is valid" >::
+  assert_valid true "9999999999 9999999999 9999999999 9999999999";
+  "valid luhn with an odd number of digits and non zero first digit" >::
+  assert_valid true "109";
   "using ascii value for non-doubled non-digit isn't allowed" >::
   assert_valid false "055b 444 285";
   "using ascii value for doubled non-digit isn't allowed" >::
   assert_valid false ":9";
+  "non-numeric, non-space char in the middle with a sum that's divisible by 10 isn't allowed" >::
+  assert_valid false "59%59";
 ]
 
 let () =
