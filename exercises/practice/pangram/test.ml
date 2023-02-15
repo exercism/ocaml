4c0050a0e30c002d7a839440867cc9ce864412fe
George Shammas <george@shamm.as>
2023-02-15 10:51:43 -0500

    pangram: Sync with problem-specfications

--- exercises/practice/pangram/test.ml
+++ exercises/practice/pangram/test.ml
@@ -1,30 +1,251 @@
-(* pangram - 1.4.1 *)
 open OUnit2
 open Pangram
 
 let ae exp got _test_ctxt = assert_equal ~printer:string_of_bool exp got
 
 let tests = [
-  "sentence empty" >::
+  "empty sentence" >::
   ae false (is_pangram "");
-  "recognizes a perfect lower case pangram" >::
+  "perfect lower case" >::
   ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
-  "pangram with only lower case" >::
+  "only lower case" >::
   ae true (is_pangram "the quick brown fox jumps over the lazy dog");
-  "missing character 'x'" >::
+  "missing the letter 'x'" >::
   ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
-  "another missing character, e.g. 'h'" >::
+  "missing the letter 'h'" >::
   ae false (is_pangram "five boxing wizards jump quickly at it");
-  "pangram with underscores" >::
+  "with underscores" >::
   ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
-  "pangram with numbers" >::
+  "with numbers" >::
   ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
   "missing letters replaced by numbers" >::
   ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
-  "pangram with mixed case and punctuation" >::
+  "mixed case and punctuation" >::
   ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
-  "upper and lower case versions of the same character should not be counted separately" >::
+  "case insensitive" >::
   ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
+  "empty sentence" >::
+  ae false (is_pangram "");
+  "perfect lower case" >::
+  ae true (is_pangram "abcdefghijklmnopqrstuvwxyz");
+  "only lower case" >::
+  ae true (is_pangram "the quick brown fox jumps over the lazy dog");
+  "missing the letter 'x'" >::
+  ae false (is_pangram "a quick movement of the enemy will jeopardize five gunboats");
+  "missing the letter 'h'" >::
+  ae false (is_pangram "five boxing wizards jump quickly at it");
+  "with underscores" >::
+  ae true (is_pangram "the_quick_brown_fox_jumps_over_the_lazy_dog");
+  "with numbers" >::
+  ae true (is_pangram "the 1 quick brown fox jumps over the 2 lazy dogs");
+  "missing letters replaced by numbers" >::
+  ae false (is_pangram "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
+  "mixed case and punctuation" >::
+  ae true (is_pangram "\"Five quacking Zephyrs jolt my wax bed.\"");
+  "case insensitive" >::
+  ae false (is_pangram "the quick brown fox jumps over with lazy FX");
+  "a-m and A-M are 26 different characters but not a pangram" >::
+  ae false (is_pangram "abcdefghijklm ABCDEFGHIJKLM");
 ]
 
 let () =
