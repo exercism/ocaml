524bc72756c41c815cef7b0b1935cf9735775fa8
Owen Rees <owenrees.dev@gmail.com>
2025-09-15 16:28:26 +0200

    Fix word count example to pass apostrophe tests (#560)

--- exercises/practice/word-count/.meta/example.ml
+++ exercises/practice/word-count/.meta/example.ml
@@ -7,25 +7,14 @@ let normalize = function
   | ch when Char.is_alphanum ch || Char.equal ch '\'' -> Char.lowercase ch
   | _ -> ' '
 
-type position =
-  | Start
-  | End
-
-let quote_at s ~pos =
-  let p = match pos with
-  | Start -> 0
-  | End -> String.length s - 1
-  in
-  String.is_substring_at s ~pos:p ~substring:"'"
+let remove_quotes word =
+  String.strip word ~drop:(Char.equal '\'')
 
 let word_count s =
   let s = String.map s ~f:normalize in
   let split =
-    List.filter (String.split s ~on:' ') ~f:(Fn.non String.is_empty)
-    |> List.map ~f:(fun w ->
-      let len = String.length w in
-      if len >= 2 && quote_at w ~pos:Start && quote_at w ~pos:End
-      then String.sub w ~pos:1 ~len:(len - 2)
-      else w)
+    String.split s ~on:' '
+    |> List.map ~f:remove_quotes
+    |> List.filter ~f:(Fn.non String.is_empty)
   in
   List.fold ~init:(Map.empty (module String)) ~f:add_to_map split
